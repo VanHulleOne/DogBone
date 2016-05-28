813889dc23b54f37fa2d24a08fb8615b4d8804e0
die-bold <a.s.diebold@gmail.com>
2016-05-28 17:52:13 -0500

    Add files via upload
    Been researching how to make a GUI. Starting small and just adding an entry widget. Will be adding much more soon.

--- gui.py
+++ gui.py
@@ -6,3 +6,22 @@ Created on Sat May 28 16:39:58 2016
 """
 
 from tkinter import *    #GUI module
+
+#function to make an entry widget
+def make_entry(parent, caption):
+    print(fill)
+    
+def print_content():
+    print(e.get())
+    
+#only works if program is used as the main program, not as a module    
+if __name__ == '__main__':    
+    root = Tk()         #creates GUI
+    root.title("3D Printer Parameter Setter")
+
+    e = Entry(root)
+    e.pack()
+    
+    
+    
+    root.mainloop()    
