1eadf745a360d96e3301a86549aeb0038b5c7f5d
die-bold <a.s.diebold@gmail.com>
2016-06-06 16:22:49 -0500

    Add files via upload
    Followed tutorial to create basic GUI. Format of this GUI will work well for project. Ready to make GUI that collects the needed variables.

--- gui.py
+++ gui.py
@@ -8,24 +8,33 @@ Created on Sat May 28 16:39:58 2016
 from tkinter import *    #GUI module
 
 #function to make an entry widget
-def make_entry(parent, caption):
-    print(fill)
+#def make_entry(parent, caption):
+#    print(fill)
     
-def print_content():
-    print(e.get())
+#def print_content():
+#    print(e.get())
+    
+def save():
+    label4 = Label(root,text='success').grid(row=4,column=2)
     
 #only works if program is used as the main program, not as a module    
-if __name__ == '__main__':    
-    root = Tk()         #creates GUI
-    root.title("3D Printer Parameter Setter")
-    root.geometry("500x500+100+100")
-    
-    labeltext = StringVar()
-    labeltext.set("Enter here")
-    label1 = Label(root, textvariable = labeltext, height = 4)
-    e = Entry(root, textvariable = labeltext)
-    label1.pack(side=LEFT)
-    e.pack(side=RIGHT)
-    
-    
-    root.mainloop()    
+#if __name__ == '__main__':    
+root = Tk()         #creates GUI
+root.title("3D Printer Parameter Setter")
+root.geometry("400x400+100+100")
+
+one=StringVar()
+two=StringVar()
+
+labeltext = StringVar()
+labeltext.set("Enter here")
+label1 = Label(root, text = "Enter here:", height = 4).grid(row=0,column=2)
+label2 = Label(root, text = "Enter:", height = 4).grid(row=1,column=0)
+label3 = Label(root, text =  "here:", height = 4).grid(row=2,column=0)
+
+e1 = Entry(root, textvariable=one).grid(row=1,column=2)
+e2 = Entry(root, textvariable=two).grid(row=2,column=2)
+
+button1=Button(root,text="Submit",command=save).grid(row=3,column=2)
+
+root.mainloop()    
